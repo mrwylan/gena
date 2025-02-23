f0f39654f86eb7fa4fb84c776fd792b518ec8ad9
Jakob Albrecht <mrwylan@gmail.com>
2025-02-23 15:36:09 +0100

    from spring boot initializer

---
+++ src/main/java/info/wylan/gena/GenaApplication.java
@@ -0,0 +1,13 @@
+package info.wylan.gena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenaApplication.class, args);
+	}
+
+}
