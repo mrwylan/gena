c539dce1a069fd89617d779001c0e17116215e22
Jakob Albrecht <mrwylan@gmail.com>
2025-02-23 15:59:30 +0100

    after merge

---
+++ src/test/java/info/wylan/gena/GenaApplicationTests.java
@@ -0,0 +1,13 @@
+package info.wylan.gena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
